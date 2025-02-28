41e9172241a17297b5b9456f24971e967d2b4bf7
ASalazar11 <106692697+ASalazar11@users.noreply.github.com>
2025-02-27 21:30:08 -0800

    Update pdf_generator.py

--- app/pdf_generator.py
+++ app/pdf_generator.py
@@ -268,7 +268,7 @@ def generate_pdf(request):
         texto_pejv = """Quien suscribe, portador de la cédula 7-0054-0131, como APODERADO GENERALÍSIMO SIN LÍMITE DE SUMA DE LIMOFRUT S.A., cédula jurídica 3-101-297878 en calidad de Asegurado del vehículo placa CL-292788 el PODERDANTE, otorgó PODER ESPECIAL de conformidad con el artículo mil doscientos cincuenta y seis del Código Civil de la República de Costa Rica a favor de la señora Krisby Wabe Arce, mayor, soltera, vecina de Curridabat, con número de cédula 1-112190411 y/o Mirkala Wabe Arce, mayor, soltera, vecina de Curridabat, con número de cédula 1-10990472, y/o David Matamoros Rojas, mayor, casado, vecino de Cartago, con número de cédula 1-10650005, pudiendo actuar conjunta o separadamente, funcionarios del taller Wabe, Carrocería y Pintura, Sociedad Anónima, cédula de persona jurídica 3-101-085331, en lo sucesivo los APODERADOS, les faculto para que en mi representación realicen gestiones ante cualesquiera de las instalaciones o departamentos del Instituto Nacional de Seguros."""
 
         # Llamar a la función para dibujar el texto con saltos de línea automáticos
-        draw_wrapped_text(c3, texto_pejv, 60, 810, 500)  # x=100, y=700, ancho máximo=400px
+        draw_wrapped_text(c3, texto_pejv, 60, 790, 500)  # x=100, y=700, ancho máximo=400px
 
         # Guardar y posicionar el documento
         c3.save()
